9698435152cd8b12df8b2121bee6f6621fa74e8b
Mikel Arnaiz <mikel.arnaiz@gmail.com>
2019-03-15 13:33:30 +0100

    Sets background color

--- src/js/options.js
+++ src/js/options.js
@@ -6,7 +6,9 @@
 
 let page = document.getElementById("buttonDiv");
 
-const kButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
+const kButtonColors = ["#1E1E1E", "#e8453c", "#f9bb2d", "#4688f1"];
+
+// 6e9a79
 
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
--- src/js/popup.js
+++ src/js/popup.js
@@ -13,8 +13,11 @@ chrome.storage.sync.get("color", function(data) {
 changeColor.onclick = function(element) {
   let color = element.target.value;
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.backgroundColor = "' + color + '";'
-    });
+    // chrome.tabs.executeScript(tabs[0].id, {
+    //   code: 'document.body.style.backgroundColor = "' + color + '";'
+    // });
+
+    // Routes are relative to the root
+    chrome.tabs.insertCSS((tabs[0].id, { file: "./styles/styles.css" }));
   });
 };
