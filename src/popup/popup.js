501227c51d1c5bcabb1a5fd4f1cfae2b60b63c7c
Mikel Arnaiz <mikel.arnaiz@gmail.com>
2019-03-15 15:08:58 +0100

    Renames styles file

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -5,7 +5,9 @@ let changeColorButton = document.getElementById("changeColor");
 
 changeColorButton.onclick = function() {
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    chrome.tabs.insertCSS((tabs[0].id, { file: "./styles/styles.css" }));
+    chrome.tabs.insertCSS(
+      (tabs[0].id, { file: "./styles/changeBackground.css" })
+    );
     chrome.tabs.executeScript(tabs[0].id, {
       code: `
         document.body.classList.toggle('dark-pr');
