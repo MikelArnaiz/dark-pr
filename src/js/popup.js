f5952543253fca1640d1bf06afaaefc08522eb80
Mikel Arnaiz <mikel.arnaiz@gmail.com>
2019-03-15 15:02:43 +0100

    Clean up

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,23 +1,17 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 "use strict";
 
-let changeColor = document.getElementById("changeColor");
-chrome.storage.sync.get("color", function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute("value", data.color);
-});
+let isActive = false;
+let changeColorButton = document.getElementById("changeColor");
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    // chrome.tabs.executeScript(tabs[0].id, {
-    //   code: 'document.body.style.backgroundColor = "' + color + '";'
-    // });
-
-    // Routes are relative to the root
+changeColorButton.onclick = function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     chrome.tabs.insertCSS((tabs[0].id, { file: "./styles/styles.css" }));
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: `
+        document.body.classList.toggle('dark-pr');
+      `
+    });
+    isActive = !isActive;
+    changeColorButton.innerHTML = isActive ? "Turn off" : "Turn on";
   });
 };
